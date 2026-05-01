aec5e7b5b111d4a937f5edd57fe117e1240e0949
duwon <webmind7878@gmail.com>
2026-05-02 03:05:51 +0900

    chore: standardize bootstrap assets

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootswatch/dist/vapor/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
